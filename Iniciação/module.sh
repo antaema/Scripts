25fff7a3c99d5401f931cb261f03a0e5ba59a9e2
Emanoel Bruno <30728565+antaema@users.noreply.github.com>
2017-10-03 09:29:06 -0300

    Update module.sh

--- Iniciação/module.sh
+++ Iniciação/module.sh
@@ -6,6 +6,15 @@ if [[ $# != 1 ]]; then
 elif [[ ! -f $1 ]]; then
 	echo "Arquivo de descricao invalido ou nao existe"
 fi
+
 OldIFS=$IFS;
 IFS=';';
-for line in $1 
+
+for line in $(cat $1); do
+   IFS=' ';
+   for token in $line; do
+      echo $token;
+   done
+   IFS=';';
+done
+
