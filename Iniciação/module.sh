cdee1513e2cb42dba28c903bd4ccbbd1aa6c6784
antaema <emanoelbruno22@gmail.com>
2017-10-05 09:14:51 -0300

    testando

--- Iniciação/module.sh
+++ Iniciação/module.sh
@@ -9,18 +9,35 @@ fi
 
 OldIFS=$IFS;
 IFS=';';
+state='0';
 
 for line in $(cat $1); do
-      IFS=' ';
-      line=$(echo $line | tr -d '\n');
-      for token in $line; do       
-      		IFS=',';
-      		for subToken in $token; do
-               if [[ -n "$subToken" ]]; then
-         		 echo $subToken;
-               fi 
-         	done
-      done
-   IFS=';';
+	IFS=' ';
+	line=$(echo $line | tr -d '\n');
+	for token in $line; do       
+		IFS=',';
+		for subToken in $token; do
+			IFS=$OldIFS;
+			case $state in
+			0)		  
+				echo $subToken;
+				if [[ "$subtoken" == "executable" ]];then
+					echo "1";
+				elif [[ "$subtoken" == "10%"  ]];then
+					echo "2";
+				else
+					echo "3";
+				fi
+			;;
+			1) 
+				echo "Um" 
+			;;
+			*) 
+				echo "Erro de sintaxe na linha $line" 
+			;;
+			esac
+		done
+	done
+	IFS=';';
 done
 
