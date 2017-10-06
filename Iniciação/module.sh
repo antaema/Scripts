6c97917bcd047841877aeaa7728bff8d45dacc0f
antaema <emanoelbruno22@gmail.com>
2017-10-06 09:27:39 -0300

    Update

--- Iniciação/module.sh
+++ Iniciação/module.sh
@@ -8,36 +8,117 @@ elif [[ ! -f $1 ]]; then
 fi
 
 OldIFS=$IFS;
-IFS=';';
-state='0';
+IFS=$'\n';
 
 for line in $(cat $1); do
-	IFS=' ';
-	line=$(echo $line | tr -d '\n');
-	for token in $line; do       
-		IFS=',';
-		for subToken in $token; do
-
-			case $state in
-			0)		  
-				echo $subToken;
-				if [[ "$subtoken" == "executable" ]];then
-					echo "1";
-				elif [[ "$subtoken" == "10%"  ]];then
-					echo "2";
+	state=0;
+	step=0;
+	first=false
+	IFS=', ';
+	for token in $line; do 
+		echo $token;
+		if [[ $first == "false" ]];then
+			first=true;
+			if [[ $token == //* ]];then
+				break;
+			fi
+		fi
+		case $state in
+		0)		  
+			if [[ "$token" == "program" ]];then
+				state=1;
+			elif [[ "$token" == "cpu_limit"  ]];then
+				state=2;
+			elif [[ "$token" == "memory_limit"  ]];then
+				state=3;
+			elif [[ "$token" == "exit_events"  ]];then
+				state=4;
+			else
+				echo "Erro de sintaxe na linha \"$line\"" ;
+				break;
+			fi
+		;;
+		1) 	  
+			if [[ "$token" == "=" ]];then
+				step=1;
+			elif [[ $step == "1" ]];then
+				if [[ -f $token ]];then
+					programa=$token;
+					step=2;
 				else
-					echo "3";
+					echo "Erro na linha \"$line\"";
+					echo "Progama inexistente"
+					break;
 				fi
-			;;
-			1) 
-				echo "Um" 
-			;;
-			*) 
-				echo "Erro de sintaxe na linha $line" 
-			;;
-			esac
-		done
+			else
+				echo "Erro na linha \"$line\"";
+			 	break;
+			fi
+		;;
+		2) 	  
+			if [[ "$token" == "=" ]];then
+				step=1;
+			elif [[ $step == "1" ]];then
+				token=$(echo $token | sed 's/%//');
+				if [[ $token =~ ^[0-9]+([.,]?[0-9]+)?$ ]];then
+					teste=$(echo "$token <= 100.0" | bc);
+					if [[ $teste == 1 ]];then
+						percentage_cpu=teste;
+						step=2;
+					else
+						echo "Erro na linha \"$line\"";
+						echo "Valor maior que 100%"
+						break
+					fi
+				else
+					echo "Erro na linha \"$line\"";
+					echo "Progama inexistente"
+					break;
+				fi
+			else
+				echo "Erro na linha \"$line\"";
+			 	break;
+			fi
+		;;
+		3) 	  
+			if [[ "$token" == "=" ]];then
+				step=1;
+			elif [[ $step == "1" ]];then
+				token=$(echo $token | sed 's/%//');
+				if [[ $token =~ ^[0-9]+([.,]?[0-9]+)?$ ]];then
+					teste=$(echo "$token <= 100.0" | bc);
+					if [[ $teste == 1 ]];then
+						percentage_memory=teste;
+						step=2;
+					else
+						echo "Erro na linha \"$line\"";
+						echo "Valor maior que 100%"
+						break
+					fi
+				else
+					echo "Erro na linha \"$line\"";
+					echo "Progama inexistente"
+					break;
+				fi
+			else
+				echo "Erro na linha \"$line\"";
+			 	break;
+			fi
+		;;
+		4) 	  
+			if [[ "$token" == "=" ]];then
+				step=1;
+			elif [[ $step == "1" ]];then
+				if [[ $token == "keyboard" ]];then
+					keyboard=true;
+				else
+					echo "Erro na linha \"$line\"";
+					echo "Progama inexistente"
+					break;
+				fi
+			fi
+		;;		
+		esac
 	done
-	IFS=';';
 done
 
